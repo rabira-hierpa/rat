5541b07db70d7041e174fba94ef282c92a162efd
Rabra Hierpa <rabira.hierpa@gmail.com>
2022-02-23 12:44:19 +0300

    📦 added ant design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Button  from "antd/es/button";
 import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="w-1/2 h-full flex flex-col justify-center space-x-5">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Rat!</Button>
     </div>
   );
 }
